0ed1f52f4d6f52cef0e8a48821d5904ba442342b
dev-jay-tech-ai <jaewonaaart@gmail.com>
2024-03-07 21:36:14 +0000

    Life circle and useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import styled from 'styled-components'
 
 const DetailPage = ({ shoes }) => {
-
     const { id } = useParams()
+    const [alert, setAlert] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => { setAlert(false) }, 2000)
+        return () => clearTimeout(timer)
+    },[])
 
     return (
         <div className='container'>
+            { alert && <div className='alert alert-warning'>2초 이내 구매시 할인</div> }
             <div className='row'>
                 <div className='col-md-6'>
-                    <img src={'https://codingapple1.github.io/shop/shoes'+ (Number(id)+1) + '.jpg'} />
+                    <img src={'https://codingapple1.github.io/shop/shoes'+ (Number(id)+1) + '.jpg'} 
+                    width='100%'
+                    />
                 </div>
                 <div className='col-md-6 mt-4'>
                     <h4 className='pt-5'>{shoes[id].title}</h4>
